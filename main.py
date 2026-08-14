b6c1f064228818eebfe609684663a1088dafc5f6
Abhishek Rajbhar <rajbharabhishek011@gmail.com>
2026-08-14 19:57:39 +0530

    Lesson 1 done

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 class BankAccount:
-    def __init__(self):
-        self.balance = 0
+    def __init__(self, owner, balance = 0):
+        self.balance = balance
+        self.owner = owner
     # Implement deposit, withdraw
 
-account = BankAccount()
+    def deposit(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise ValueError("insufficient funds")
+        self.balance -= amount
+
+account = BankAccount("Alice")
 import sys
 for line in sys.stdin:
     parts = line.strip().split()
@@ -11,3 +19,20 @@ for line in sys.stdin:
         continue
     cmd = parts[0]
     # Handle the three commands
+
+    if cmd == "deposit":
+        amount = int(parts[1])
+        account.deposit(amount)
+
+    elif cmd == "withdraw":
+        amount = int(parts[1])
+        try:
+            account.withdraw(amount)
+        except ValueError as e:
+            print(f"{e}")
+
+    elif cmd == "balance":
+        print(account.balance)
+
+    else:
+        print(f"Unknown command: {cmd}")
