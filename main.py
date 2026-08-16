687a2ec0c705313cff4a4a2e3b0181becd9d3a82
Abhishek Rajbhar <rajbharabhishek011@gmail.com>
2026-08-16 20:38:58 +0530

    Lesson 4 done

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    # __add__ and __repr__ go here
-    def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
-    def __repr__(self):
-        return f"Vector({self.x}, {self.y})"
+def fibonacci(limit):
+    # Yield Fibonacci numbers a where a < limit
+    a,b = 0, 1
+    while a < limit:
+        yield a
+        a,b = b, a+b
+    pass
 
-x1, y1 = map(int, input().split())
-x2, y2 = map(int, input().split())
-v1 = Vector(x1, y1)
-v2 = Vector(x2, y2)
-# print(repr(v1 + v2)
-print(repr(v1 + v2))
+n = int(input())
+result = list(fibonacci(n))
+print(" ".join(str(x) for x in result))
