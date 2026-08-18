824b8fb76b1b20c061242d2cb809c98c4ed9f237
Abhishek Rajbhar <rajbharabhishek011@gmail.com>
2026-08-18 20:00:46 +0530

    Lesson 5 done

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def fibonacci(limit):
-    # Yield Fibonacci numbers a where a < limit
-    a,b = 0, 1
-    while a < limit:
-        yield a
-        a,b = b, a+b
-    pass
-
 n = int(input())
-result = list(fibonacci(n))
-print(" ".join(str(x) for x in result))
+# total = sum(...)  # use a generator expression
+# print(total)
+total = sum(x*x for x in range(1, n+1))
+print(total)
